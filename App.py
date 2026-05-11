59afd50609fe043a8601194e99d7039cb69e8b22
Harshit <harshit73010@gmail.com>
2026-05-11 12:40:56 +0530

    Add Streamlit app for Crew Mind Agent
    Implement a Streamlit app for the Crew Mind Agent that allows users to input research topics and run an agent to generate detailed reports.

---
+++ App.py
@@ -0,0 +1,49 @@
+    
+import os
+import streamlit as st
+
+from crewai import Agent, Task, Crew, LLM
+
+# API KEY
+os.environ["GROQ_API_KEY"] = "XXXXXXXXXXXXXXXXXXXXXXXXX"
+
+# LLM
+llm = LLM(
+    model="groq/llama-3.1-8b-instant",
+    api_key=os.environ["GROQ_API_KEY"]
+)
+# Page config
+st.set_page_config(page_title="Crew Mind Agent", page_icon="🤖")
+
+# Title
+st.title("🤖 Crew Mind Agent")
+st.write("Powered by Harshit 🚀")
+
+# Streamlit UI
+#st.title("Crew Mind Agent")
+
+query = st.text_input("Enter your research topic")
+
+if st.button("Run Agent"):
+
+    researcher = Agent(
+        role="Project manager",
+        goal=f"Write {query}",
+        backstory="You are an expert project manager.",
+        llm=llm
+    )
+
+    task = Task(
+        description=f"detailed writing {query}",
+        expected_output="A detailed report",
+        agent=researcher
+    )
+
+    crew = Crew(
+        agents=[researcher],
+        tasks=[task]
+    )
+
+    result = crew.kickoff()
+
+    st.markdown(result.raw)
